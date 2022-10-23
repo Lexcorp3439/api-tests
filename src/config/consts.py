0e148913ff46e5fe0f94fd5ed1477b1ee76da5e0
Romanov Aleksey Leonidovich <romanovaleks@ozon.ru>
2022-10-23 21:09:40 +0300

    Small refactoring

--- src/config/consts.py
+++ src/config/consts.py
@@ -5,5 +5,5 @@ COOKIE_HEADER = "Set-Cookie"
 RATE_LIMIT_HEADER = "X-Rate-Limit"
 EXPIRES_AFTER_HEADER = "X-Expires-After"
 
-TEXT: allure.attachment_type.TEXT
+TEXT = allure.attachment_type.TEXT
 HTML = allure.attachment_type.HTML
